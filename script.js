f5bf51a784cdc4dc44134b61c0340fb689475c94
Timothyvu99 <timothyvu99@gmail.com>
2023-09-24 21:42:45 -0700

    Functional calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function calculate(x)
+{
+    var string = document.getElementById('screen');
+    string.value += x;
+}
+
+function clear()
+{
+    document.getElementById('screen').value = '';
+}
+
+function equal()
+{
+    var solution = eval(document.getElementById('screen').value);
+    var result = document.getElementById('screen');
+
+    result.value = solution;
+}
